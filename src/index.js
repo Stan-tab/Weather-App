656126747f284675c34f3726b4d0de495d3a7b87
Stan-tab <godofwarmafia2@gmail.com>
2025-03-10 16:49:32 +0500

    feat: Final commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import './style.css';
-import weather from '../../some.json'; //example of data
 import { errorHandle, data } from './data.js';
 
 // getPromice
 (async () => {
 	const top = document.querySelector('.location');
 	const paras = createElements(2, 'p');
+	const mainBox = document.querySelector('.mainBox');
 	paras.forEach((e) => top.appendChild(e));
 	paras[0].textContent = 'Please wait...';
-	// const weather = await errorHandle(data.weather)(await data.getLocation());
+	const weather = await errorHandle(data.weather)(await data.getLocation());
 	try {
 		headerDealer(paras, weather);
-	} catch {
+		weather.days.forEach((el) => {
+			mainBox.appendChild(addCard(el));
+		});
+	} catch (e){
+		console.log(e)
 		paras[0].textContent = 'Sorry, try after few minutes';
 		return;
 	}
@@ -34,4 +38,30 @@ function headerDealer(paras, weather) {
 	} else {
 		paras[1].textContent = `${weather.current.conditions} day in ${weather.location}`;
 	}
+}
+
+function addCard(data) {
+	const date = new Date(data.datetime);
+	const days = [
+		'Sunday',
+		'Monday',
+		'Tuesday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Saturday'
+	];
+	const div = document.createElement('div');
+	div.classList = "card";
+	const paras = createElements(4, 'p');
+	paras[0].textContent = days[date.getDay()];
+	paras[1].innerHTML = `${data.tempmax}&deg; ${data.tempmin}&deg;`;
+	paras[2].textContent = data.conditions;
+	paras[3].textContent = `${date.getDate()}/${+date.getMonth() + 1}`;
+
+	for (let i = 0; i < 4; i++) {
+		div.appendChild(paras[i]);
+	}
+
+	return div;
 }
